866ca63ce5c7f5ae8074d18c2d58cf835a889bbd
xiaohang zou <xzou000@citymail.cuny.edu>
2018-04-06 18:33:26 -0400

    get data from database place to calendar

--- client/src/app/pickup-requests/pickup-requests.component.ts
+++ client/src/app/pickup-requests/pickup-requests.component.ts
@@ -1,51 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-import 'moment';
-import 'fullcalendar';
+import { Component, OnInit, ChangeDetectorRef, Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                 //api
+//import * as $ from 'jquery';
+//import { WasteRequestService } from '../services/waste-request.service';
+import { Observable } from 'rxjs/Observable';
+import { element } from 'protractor';
 
 
 @Component({
-  selector: 'app-pickup-requests',
-  templateUrl: './pickup-requests.component.html',
-  styleUrls: ['./pickup-requests.component.css'],
-  // encapsulation: ViewEncapsulation.None
+    selector: 'app-pickup-requests',
+    templateUrl: './pickup-requests.component.html',
+    styleUrls: ['./pickup-requests.component.css'],
+    // encapsulation: ViewEncapsulation.None
 })
 export class PickupRequestsComponent implements OnInit {
-  events: any[];
-  header: any;
-  dialogVisible: boolean = false;
-  constructor() { }
-
-  ngOnInit() {
-    this.events = [
-      {
-          "title": "All Day Event",
-          "start": "2018-03-28"
-      },
-      {
-          "title": "Long Event",
-          "start": "2018-03-07",
-          "end": "2018-03-10"
-      },
-      {
-          "title": "Repeating Event",
-          "start": "2018-03-09T16:00:00"
-      },
-      {
-          "title": "Repeating Event",
-          "start": "2018-03-16T16:00:00"
-      },
-      {
-          "title": "Conference",
-          "start": "2018-03-11",
-          "end": "2018-03-30"
-      }
-  ];
-    this.header = {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-  };
-  }
-
+    requests: Array<Object> = [
+        {'title':'Title1', 'request_id':'18E0001','department': 'CHEM', 'operator': 'Jane Doe', 'start': '2018-04-14T14:00:00', 'end': "2018-04-15T14:00:00", 'description': 'good event blablalala'},
+        {'title':'Title2',request_id:'18E0002',department: 'PHYS', operator: 'John Smith', requested: '3/5/2018', 'start': '2018-04-10T14:00:00', 'end': "2018-04-12T14:00:00",description: 'good event'},
+        {'title':'Title2',request_id:'18E0003',department: 'CHEM', operator: 'Bob Ross', requested: '3/5/2018', 'start': '2018-04-18T14:00:00', 'end': "2018-04-15T14:00:00",description: 'good event'}
+    ];
+    headerConfig: any;
+    dialogVisible: boolean = false;
+    constructor(/* private wasteRequestService: WasteRequestService,  */ private cd: ChangeDetectorRef) { }  
+    /* 
+    getData(){
+        this.wasteRequestService.getRequests().subscribe((data)=>{
+            if (data) {
+                this.requests = {
+                    id:  this.wasteRequestService.getRequests().id,
+                    url: None,
+                    title: this.wasteRequestService.getRequests().department + " " + this.wasteRequestService.getRequests().operator,
+                    start: this.wasteRequestService.getRequests().requested,        //date
+                    backgroundColor: #FFFFFFF,
+                    textColor: #0000000,
+        }
+    } */
+    /* eventRender(event,element) {
+        element.popover({
+            title: "title",
+            content: 'event.description',
+            trigger: 'hover',
+            placement: 'top',
+            container: 'body'
+          });
+    } */
+    ngOnInit() {
+        
+        this.headerConfig = {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        };
+        //this.getData();
+    }
+    
 }
+
--- client/src/app/services/waste-request.service.ts
+++ client/src/app/services/waste-request.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
-
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class WasteRequestService {
+  domain = "http://localhost:3000";
+  constructor(private http: HttpClient) {
+  }
+  
 
-  constructor() { }
+  getRequests(): Promise<any>{
+  let promise = new Promise((resolve,reject) => {
+    this.http.get(this.domain + '/ehos/event').toPromise()
+    .then(
+      res => { // Success
+        resolve(res);
+     },
+     msg => { // Error
+       reject(msg);
+     });
+ });
+ return promise;
+  /* getRequests(): Observable<any>{
+    return this.http.get(this.domain + '/ehos/requests');
+  } */
+}
 }
