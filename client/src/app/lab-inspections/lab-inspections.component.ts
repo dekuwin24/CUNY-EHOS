b763796c37cb7ec32684a768c3c79daa696ef812
xiaohang zou <xzou000@citymail.cuny.edu>
2018-04-23 01:24:06 -0400

    not finish

--- client/src/app/lab-inspections/lab-inspections.component.ts
+++ client/src/app/lab-inspections/lab-inspections.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as moment from 'moment';
+import { AccordionModule } from 'primeng/accordion'; 
+import { Message } from 'primeng/components/common/api';
+import { MessageService } from 'primeng/components/common/messageservice';
+import { UserService } from "../services/user.service";
+import {OverlayPanelModule} from 'primeng/overlaypanel';
 @Component({
   selector: 'app-lab-inspections',
   templateUrl: './lab-inspections.component.html',
@@ -7,13 +13,29 @@ import { Component, OnInit } from '@angular/core';
   // encapsulation: ViewEncapsulation.None
 })
 export class LabInspectionsComponent implements OnInit {
+    inspectors: any[];
+    inspection: any;
+    date: Date;
+    Room: Number;
+    User: Number;
+    constructor(private messageService: MessageService, private user: UserService) { 
 
-  constructor( ) { 
-
-  }
-
-  ngOnInit() {
-  
-  }
-
+    }
+    scheduleInspection() {
+        this.inspection = {
+          EhosId: this.inspection, // the pickup requester
+          start: moment(this.date).format(),
+          eventType: 2,
+          serviced: false
+      }
+    }
+    ngOnInit() {
+        this.user.getUsers().then(response => {// get EHOS members
+            this.inspectors = response;
+          }).catch(reason => {
+            console.log(reason);
+      });
+    }
 }
+
+
